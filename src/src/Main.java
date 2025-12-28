19595be2830b39957549d38a02c1b55543ce8c17
Parsa <parsarahimian85@gmail.com>
2025-12-27 21:40:31 -0500

    remove .class files

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is the start of the Huka game");
+        System.out.println("This is the start of the Huka gamegit rm -r --cached out\n");
     }
 }
