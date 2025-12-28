ba095339ef8167df6f1c94de13e769a789e95567
Parsa <parsarahimian85@gmail.com>
2025-12-28 00:46:30 -0500

    modification on Game"

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -1,5 +1,44 @@
 package Model;
+import Model.Deck.Deck;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
+    private Deck deck;
+    private int PlayerTurn;
+    private ArrayList<Player> players = new ArrayList<>();
+
+
+    public Game()
+    {
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("How many Player are in the game?");
+            int number = scanner.nextInt();
+            if (number >= 6 &&  number <= 2) {
+                System.out.println("Try again!");
+                break;
+            };
+        }
+        InitilizeAllPlayers();
+
+
+    }
+
+    private void InitilizeAllPlayers()
+    {
+        for(Player player: players)
+        {
+            player.PlayerInitializer(new ArrayList<>(java.util.Arrays.asList(
+                    deck.draw(), deck.draw(), deck.draw().faceUp(), deck.draw().faceUp()
+            )));
+        }
+
+
+
+
+    }
 
 }
