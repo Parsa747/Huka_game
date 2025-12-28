5523e7bf00919e3e3eade30d02e9ee57cef48956
Parsa <parsarahimian85@gmail.com>
2025-12-27 23:17:16 -0500

    Player has been implimented

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,6 +1,45 @@
 package Model;
 
 
+import Model.Deck.Card;
+
+import java.util.ArrayList;
+
 public class Player {
+    private ArrayList<Card> playerCards;
+    private String name;
+    private int playerHandValue;
+
+    public Player(String name, String playerHandValue)
+    {
+        playerCards = new ArrayList<>();
+        this.name = name;
+        this.playerHandValue = 0;
+
+    }
+    public void PlayerInitializer(Card card)
+    {
+        this.playerCards.add(card);
+    }
+    public int GetPlayerCardValues(){
+        return this.playerHandValue;
+    }
+
+    public void PlayerReceiveCard(Card card)
+    {
+        this.playerCards.add(card);
+    }
+    public void PLayerRemoveCard(Card card)
+    {
+        int cardValue = card.getValue();
+        for(Card card1 : this.playerCards)
+        {
+            if(card1.getValue() == cardValue){
+                this.playerCards.remove(card);
+            }
+        }
+    }
+
+
 
 }
