d65964a48870890d7d58371d9830a8da336601db
Parsa <parsarahimian85@gmail.com>
2025-12-27 22:58:56 -0500

    continue

--- src/Model/Deck/Deck.java
+++ src/Model/Deck/Deck.java
@@ -1,5 +1,6 @@
 package Model.Deck;
-
+import java.util.Random;
+import java.util.random.*;
 import java.util.ArrayList;
 
 public class Deck {
@@ -7,6 +8,33 @@ public class Deck {
 
     public Deck(){
         this.cards = new ArrayList<>();
+    }
+    public void creatDeck()
+    {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("King");
+        names.add("Queen");
+        names.add("Soldiers");
+
+        for(String name: names){
+            for(int i = 1; i <= 10; i++){
+
+                cards.add(new Card(name,i));
+            }
+        }
+        cards.add(new Card("Jocker",-1));
+        cards.add(new Card("Jocker",-1));
+
+
+    }
+    public Card draw()
+    {
+
+        Random random = new Random();
+        int randomZeroBased = random.nextInt(this.cards.size());
+        int randomOneBased = randomZeroBased + 1;
+
+        return this.cards.get(randomZeroBased);
 
     }
 }
